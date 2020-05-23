e965eb69a028527468ce32ef673ac68d7a126f6c
lif <1835304752@qq.com>
2020-05-23 16:58:20 +0800

    fir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+    use neutrino::{App, Window};
+
+    fn main() {
+        let mut window = Window::new();
+        window.set_title("Hello World");
+        window.set_size(320, 240);
+    
+        App::run(window);
+    }
+
